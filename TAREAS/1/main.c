0e48db31a2d17e5eab083baaae08b199e5fb67ff
Alexa <alexatorresm2909@gmail.com>
2019-10-15 01:59:05 -0500

    tarea1y2

---
+++ TAREAS/1/main.c
@@ -0,0 +1,9 @@
+# include <stdlib.h>
+int main(int argc, char *argu [])
+{
+    //recibe una palabra y la muestra
+    char *a;
+    a= argu[1];
+    printf("%s\n", a);
+    return 0;
+}
---
+++ TAREAS/2/main.c/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+# include <stdlib.h>
+// recibe un numero e indica si es par o non 
+int main(int argc, char *argu [])
+{
+   int variable;
+   variable= atoi(argu[1]);
+   if (variable %2==0)
+   {
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+    return 0;
+}
