c8b39d0683478e23243ab8973741d7526e126104
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-10-29 21:32:42 -0600

    Create main.c

---
+++ TAREAS/6/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char*argv[]{
+int s=1; //aqui empece a mostrar que incio en 0
+int r,j,k;//estas son las variables que se van a utilizar
+for(int i=1;i<argc;i++){//aqui empezamo a mostrar
+j=atoi(argv[i]);
+s=s*j;
+}
+for8int w=1;w<argc;w++){//aqui empezamos a checar
+r=s/k;
+printf("%i\n",r);
+}
+return 0;
+}
