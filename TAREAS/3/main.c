487153735bd87c911d5a81f71ea2a81b6a2f8193
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-10-29 20:54:40 -0600

    Update main.c

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char*argv[])
+int main (int argc, char const * argv[])
 {
-	double n1, n2;
-	n1=atof(argv[1]);
-	n2=atof(argv[2]);
-	
-	printf("%.2lf\n", n1+n2);
-	printf("%.2lf\n", n1-n2);
-	printf("%.2lf\n", n1*n1);
-	printf("%.2lf\n", n1/n2);
-	
-	return 0;
+	flotador n1,n2;
+	n1 = atof (argv[1]);
+	n2 = atof (argv[2]);
+	flotador suma, resta, multiplicacion, division;
+	suma = n1+n2;
+	resta = n1-n2;
+	multiplicacion = n1*n2;
+	division = n1/n2;
+	printf ("%.2f\n", suma);
+	printf ("%.2f\n", resta);
+	printf ("%.2f\n", multiplicacion);
+	printf ("%.2f\n", division);
+	devuelve 0;
 }
