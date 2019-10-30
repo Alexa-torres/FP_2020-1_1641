67879f76b1b39ef233b319239a439564d78d1e9d
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-10-29 20:14:31 -0600

    Update main.c

--- TAREAS/1/main.c
+++ TAREAS/1/main.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 # include <stdlib.h>
-int main(int argc, char const *argu [])
+int main(int argc, char const * argv [])
 {
-    printf("%s\n", argu [1]);
-    return 0;
+    printf("%s\n", argv[1]);
+    devuelve 0;
 }
