9556898559f3222acc0cf5f57ec27d61a3eb22af
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-04 10:10:36 -0600

    Update and rename TAREAS/3/main.c to  TAREAS/ 3/main.c

--- TAREAS/ 3/main.c
+++ TAREAS/ 3/main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 int main (int argc, char const * argv[])
 {
-	flotador n1,n2;
+	float n1,n2;
 	n1 = atof (argv[1]);
 	n2 = atof (argv[2]);
-	flotador suma, resta, multiplicacion, division;
+	float suma, resta, multiplicacion, division;
 	suma = n1+n2;
 	resta = n1-n2;
 	multiplicacion = n1*n2;
@@ -14,5 +14,5 @@ int main (int argc, char const * argv[])
 	printf ("%.2f\n", resta);
 	printf ("%.2f\n", multiplicacion);
 	printf ("%.2f\n", division);
-	devuelve 0;
+	return 0;
 }
