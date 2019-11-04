d97b1226baee5322efdf1616d47bc4a48ae1f6ec
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-04 10:20:51 -0600

    Update and rename TAREAS/6/main.c to  TAREAS/ 6/main.c

--- TAREAS/ 6/main.c
+++ TAREAS/ 6/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char*argv[]{
+int main(int argc, char*argv[]){
 int s=1; //aqui empece a mostrar que incio en 0
 int r,j,k;//estas son las variables que se van a utilizar
 for(int i=1;i<argc;i++){//aqui empezamo a mostrar
