252a19b35162288b15eb17fb26ce1c4a2a89fe91
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-06 22:10:47 -0600

    Update main.c.c

--- TAREAS/2/main.c.c
+++ TAREAS/2/main.c.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 int main (int argc, char * argv []){
 	int numero;
-	numero = atoi (argv [1]);
-	if (numero%2 == 0){
-		printf("1\n");
+	//el argumento se guarda en esta variable
+	numero=atoi(argv[1]);
+	//se hace un if
+	//si el modulo de num%2 es 0 es divisible entre 2
+	if(numero%2==0){
+	//se imprime uno porque es par 
+	printf("1\n");
 	}
+	//si no significa que es non
 	else{
-		printf("0\n");
+	//se imprime 0 si es non
+	printf("0\n");
 	}
 	return 0;
 }
