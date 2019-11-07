84c29fb77cf938c0255b74ca5fba68ef34af4523
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-06 22:14:07 -0600

    Update main.c.c

--- TAREAS/1/main.c.c
+++ TAREAS/1/main.c.c
@@ -2,6 +2,7 @@
 # include <stdlib.h>
 int main(int argc, char const * argv [])
 {
+    //argv[1] es la palabra que queremos repetir asi que la imprimimos
     printf("%s\n", argv[1]);
     return 0;
 }
