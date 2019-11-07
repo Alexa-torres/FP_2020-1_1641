b99bfa976420f82e555ffdf0322ce77611c60d00
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-06 21:53:27 -0600

    Update main.c.c

--- TAREAS/2/main.c.c
+++ TAREAS/2/main.c.c
@@ -4,10 +4,10 @@ int main (int argc, char * argv []){
 	int numero;
 	numero = atoi (argv [1]);
 	if (numero%2 == 0){
-		printf(" 1 \n ");
+		printf("1\n");
 	}
 	else{
-		printf(" 0 \n ");
+		printf("0\n");
 	}
 	return 0;
 }
