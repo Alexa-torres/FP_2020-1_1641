a6a8f993bbee809d408cdc4af654af4dfd2e15dd
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-07 23:27:22 -0600

    Create main.c

---
+++ TAREAS/8/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char*argu[]){
+	//declarando variables
+	float a.b.c;
+	//convirtiendo los argumentos y guardando en la variable flotante
+	a=atof(argu[1]);
+	b=atof(argu[2]);
+	//mientras a sea igual a 1 se convierte, a grados C
+	if(a==1){
+		c=(b*1.8)+32;
+		//mostrando el resultado
+		printf("%.2\n",c);
+		return 0;
+	}
+	//si a vale 0 entonces se convierte, a grados F
+	else
+		if(a==0){
+			c=(b-32)/1.8;
+			//mostrando el resultado
+			printf("%.2f\n",c);
+			return 0;
+		}
+	return 0;
+}
