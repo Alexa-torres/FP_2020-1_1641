1e67423f5bf61b7f2235f2fcd4732f6fc7fe1a26
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-09 20:17:08 -0600

    Update and rename TAREAS/8/main.c to  TAREAS/ 8/main.c

--- TAREAS/ 8/main.c
+++ TAREAS/ 8/main.c
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 int main(int argc, char*argu[]){
 	//declarando variables
-	float a.b.c;
+	float a,b,c;
 	//convirtiendo los argumentos y guardando en la variable flotante
 	a=atof(argu[1]);
 	b=atof(argu[2]);
 	//mientras a sea igual a 1 se convierte, a grados C
 	if(a==1){
-		c=(b*1.8)+32;
+		c=(b-32)*0.55555;
 		//mostrando el resultado
-		printf("%.2\n",c);
+		printf("%.2f\n",c);
 		return 0;
 	}
 	//si a vale 0 entonces se convierte, a grados F
-	else
+	else{
 		if(a==0){
-			c=(b-32)/1.8;
+			c=(b*1.8)+32;
 			//mostrando el resultado
 			printf("%.2f\n",c);
 			return 0;
 		}
+	}
 	return 0;
 }
