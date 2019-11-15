d6b5a6f95796690a490eeff64cb200a254dbe0b8
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-14 21:06:07 -0600

    Update main.c

--- TAREAS/ 5/main.c
+++ TAREAS/ 5/main.c
@@ -1,51 +1,58 @@
-#include <stdio.h>
 #include <stdlib.h>
-//esta funcion nos servira para devolver un numero de un caracter
-int valor(char n){
-	int z;
-	switch(n){
-		case 'J':
-			z=10;
-			break;
-		case 'Q':
-			z=10;
-			break;
-		case 'K':
-			z=10;
-			break;
-		case 'A':
-			z=11;
-			break;
-		case 'j':
-			z=10;
-			break;
-		case 'q':
-			z=10;
-			break;
-		case 'k':
-			z=10;
-			break;
-		case 'a':
-			z=11;
-			break;
-	}
-	return z;
+#include <stdio.h>
+char m;
+int x;
+int main(int argc, char*argv[]) {
+for (int i=1; i<argc; i++){//para que el programa vaya recorriendo los valores de entrada
+ m=argv[i][0];//se usa el cero para que en realidad tenga el valor de los argumentos y no solo los recorra
+ if(m=='J'||m=='Q'||m=='K'||m=='A'){//en caso de que el valor ingredsado seas una letra
+  switch (m){
+     case'J':
+  printf("10\n");
+ break;
+ case'Q':
+  printf("10\n");
+ break;
+ case'K':
+  printf("10\n");
+ break;
+ case'A':
+  printf("11\n");
+ }
+}else{
+  x=atoi(argv[i]);//en caso de que no sea una letra, se imprime el numero se se ingresa usando otro switch y convirtiendo el caracter a numero
+ switch(x){
+ case 2:
+  printf("2\n");
+ break;
+ case 3:
+  printf("3\n");
+ break;
+ case 4:
+  printf("4\n");
+ break;
+ case 5:
+  printf("5\n");
+ break;
+ case 6:
+  printf("6\n");
+ break;
+ case 7:
+  printf("7\n");
+  break;
+ case 8:
+  printf("8\n");
+ break;
+ case 9:
+  printf("9\n");
+ break;
+ case 10:
+  printf("10\n");
+ break;
+ default:
+ printf("Ingrese número válido\n");
 }
-int main(int argc,char*argv[]){
-	int imprimir,
-	coi;//coi significa char o int
-	for(int i=1;i<argc;i++){//se empieza a recorrer la lista en el subindice 1
-		//coi va a tener un valor booleano
-		coi=argv[i][0]=='J'||argv[i][0]=='j'||
-		argv[i][0]=='Q'||argv[i][0]=='q'||argv[i][0]=='K'
-		||argv[i][0]=='k'||argv[i][0]=='A'||argv[i][0]=='a';
-		if (coi==1){//si es un caracter a pedir se usa la funcion valor
-			imprimir=valor(argv[i][0]);
-		}
-		else{//si no basta con un atoi
-			imprimir=atoi(argv[i]);
-		}
-		printf("%i\n",imprimir);
-	}
-	return 0;
+}
+}
+return 0;
 }
