301dd1a9be792502efdadbf57ca63032cb054ba3
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-14 20:40:51 -0600

    Update main.c

--- TAREAS/11/main.c
+++ TAREAS/11/main.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>//librerias
-#include <math.h>
-double RAIZ(doublex, double tol, double candidato){//declaramos variables
-	double division, diferencia, promedio;//operacion
-	division=x/candidato;
-	diferencia=fabs(division-candidato);//es la division que se llevara a cabo
-	while(diferencia>tol){//la operacion del diferencial y el total
-	division=x/candidato;
-	diferencia=fabs(division-candidato);//se lleva a cabo la operacion
-	promedio=(candidato+division)2;)//se saca el promedio
-	candidato=promedio;//la igualdad de ambos
+//se usara una funcion llamada raiz
+double raiz(double num, double tolerancia, double resultado){//declaramos variables
+	double division, diferencia;
+	//aqui esta el algoritmo
+	division=num/resultado;
+	diferencia=resultado-division;
+	//se hace que diferencia sea con valor absoluto
+	diferencia=diferencia> 0? diferencia:-diferencia;
+	//si la diferencia es menor o igual que la tolerancia se retorna
+	//el candidato a resultado
+	if(diferencia<=tolerancia){
+		return resultado;
 	}
-	return candidato;
+	//si no
+	else{
+		//se obtiene un nuevo candidato
+		resultado=(resultado+division)/2;
+		//y se retorna la funcion con el nuevo candidato
+		return raiz(num,tolerancia,resultado);
+	}
 }
-int main(int argc, char*argv[]){//se declaran variables
-double numero,tolerancia,ans,candidato;//valores
-numero=atof(argv[1]);//operacion
-candidato=atof(argv[3]);//operacion
-tolerancia=atof(argv[2]);//operacion
-ans=RAIZ(numero,tolerancia,candidato);//la raiz toma los resultados
-printf("%lf\n",ans);//operacion
+int main(int argc, char*argu[]){//se declaran variables
+double num,tolerancia,resultado;//los argumentos se gusrdan en estas variables
+num=atof(argu[1]); tolrancia=atof(argu[2]); resultado=atof(argu[3]);
+//en esta variable se guarda el resultado
+resultado=raiz(num,tolrancia,resultado);
+//se muestra
+printf("%lf\n",resultado);
 return 0;
 }
