824a0676db50bb970e91aab420e1aed38a107543
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-14 21:09:05 -0600

    Update main.c

--- TAREAS/ 7/main.c
+++ TAREAS/ 7/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char*argv[]){
-	//declaramos variables
+#include <math.h>
+
+int main(int argc, char *argu[]) {
+//declaramos variables
 	int a;
 	int b;
 	int c=1;
-	a=atof(argv[1]);
-	b=atof(argv[2]);
-	//decimos en donde lo vamos a guardar
-	for(int i=0; i<b; i++){
-	//decimos en donde se va a guardar y que se va a ir multiplicando
-	c=c*a;
+	a=atof(argu[1]);
+	b=atof(argu[2]);
+	//Ahora vemos donde la vamos a almacenar
+	for (int i = 0; i < b; i++)
+	{
+		//decimos en donde se va a guardar y que se va a multiplicar
+		c=c*a;
 	}
 	//imprimimos el resultado
-	printf("%d\n", c);
-	return 0;
+	printf("%.2d\n",c );
+
+return 0;
 }
