077fd75eee7a952cae283a054f537da1475c2428
Alexa-torres <54477946+Alexa-torres@users.noreply.github.com>
2019-11-19 10:14:04 -0600

    Update and rename TAREAS/ 8/main.c to  TAREAS/ 8/main.c

--- TAREAS/ 8/main.c
+++ TAREAS/ 8/main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-int main(int argc, char*argu[])
-{
-	double x,y,z;
-	x=atof(argu[1]);
-	y=atof(argu[2]);
-	if(x==1)//mientras x sea 1 se convierten en grados C
-	{
-		z=(y-32)/1.8;
-		printf("%.2f\n",z);
-		return 0;
+
+int main( int argc, char *argv[]) {
+
+	int x = atoi(argv[1]);  // El primer argumento (0 o 1) se convierte a entero
+	double y = atof(argv[2]);  // El segundo argumento (grados) se convierte a double
+	double z;  // Se declara z como double
+
+	if(x==0) {
+		z = 1.8*y + 32;  // Opcion 0: convierte grados Celsius a Fahrenheit
+	}                        
+	else {
+		if (x==1) {
+			z = (y-32) / 1.8;  // Opcion 1: convierte grados Fahrenheit a Celsius
+		}
 	}
-	else if(x==0)//si x vale 0 se convierten a grados F
-	{
-		z=32+(y*1.8);
-		printf("%.2f\n",z);
-		return 0;
-	}
+
+	printf("%.2f\n", z);  // El valor convertido se guarda en la variable 'z' y se imprime
+
+
 	return 0;
 }
